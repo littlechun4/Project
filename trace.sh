17671805a7fb04b83e3873c66744cbf9f1d40300
gwolves <gwolves@snucse.org>
2013-05-19 12:54:14 +0900

    First Commit

--- trace.sh
+++ trace.sh
@@ -13,12 +13,12 @@ echo sys_write > set_graph_function
 # start logging
 echo 1 > tracing_on
 # start testing
-dd if=/dev/zero of=/home/selin/temp bs=1M count=1024 oflag=direct conv=fdatasync
+dd if=/dev/zero of=/home/gwolves/temp bs=1M count=1024 oflag=direct conv=fdatasync
 
 # save logs to file
-cat trace > /home/selin/project/trace.txt
+cat trace > /home/gwolves/Study/Project/trace.txt
 # disable logging
 echo 0 > tracing_on
 
 # remove test file
-rm /home/selin/temp
+rm /home/gwolves/temp

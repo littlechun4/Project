ff7bc60ce92d544f76ac0ba6fca68caa1b2caa94
littlechun4 <littlechun4@littlechun4-VirtualBox.(none)>
2013-12-03 16:53:56 +0900

    BASIC GUI Model

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Project
 =======
+
+12/3 
+- ui2.py: basic model of our GUI
--- sample.py
+++ sample.py
@@ -13,8 +13,19 @@ lst = []
 for val in rd.value:
 	lst += [val]
 
-p1 = win.addPlot(title = "Plot" , y = lst)
-
+p1 = win.addPlot(title="Plot" , y = lst)
+lr = pg.LinearRegionItem([30000, 70000])
+lr.setZValue(-10)
+p1.addItem(lr)
+p2 = win.addPlot(title="Region")
+p2.plot(lst)
+def updatePlot():
+	p2.setXRange(*lr.getRegion(), padding=0)
+def updateRegion():
+	lr.setRegion(p2.getViewBox().viewRange()[0])
+lr.sigRegionChanged.connect(updatePlot)
+p2.sigXRangeChanged.connect(updateRegion)
+updatePlot()
 
 if __name__ == '__main__':
     import sys

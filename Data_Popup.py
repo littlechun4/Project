d7494464df1281cd76891b982d2b9e804ec7ffc3
littlechun4 <littlechun4@naver.com>
2013-12-15 16:23:47 +0900

    Data_Popup file added

---
+++ Data_Popup.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Data.ui'
+#
+# Created: Sun Dec 15 13:14:35 2013
+#	  by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+	_fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+	_fromUtf8 = lambda s: s
+
+class Ui_Dialog(object):
+	def setupUi(self, Dialog):
+		Dialog.setObjectName(_fromUtf8("Dialog"))
+		Dialog.resize(419, 429)
+		self.pushButton = QtGui.QPushButton(Dialog)
+		self.pushButton.setGeometry(QtCore.QRect(290, 370, 98, 27))
+		self.pushButton.setObjectName(_fromUtf8("pushButton"))
+		self.label = QtGui.QLabel(Dialog)
+		self.label.setGeometry(QtCore.QRect(20, 40, 66, 17))
+		self.label.setObjectName(_fromUtf8("label"))
+		self.label_2 = QtGui.QLabel(Dialog)
+		self.label_2.setGeometry(QtCore.QRect(20, 80, 66, 17))
+		self.label_2.setObjectName(_fromUtf8("label_2"))
+		self.label_3 = QtGui.QLabel(Dialog)
+		self.label_3.setGeometry(QtCore.QRect(20, 120, 66, 17))
+		self.label_3.setObjectName(_fromUtf8("label_3"))
+		self.label_4 = QtGui.QLabel(Dialog)
+		self.label_4.setGeometry(QtCore.QRect(20, 160, 66, 17))
+		self.label_4.setObjectName(_fromUtf8("label_4"))
+		self.label_5 = QtGui.QLabel(Dialog)
+		self.label_5.setGeometry(QtCore.QRect(20, 200, 66, 17))
+		self.label_5.setObjectName(_fromUtf8("label_5"))
+		self.label_6 = QtGui.QLabel(Dialog)
+		self.label_6.setGeometry(QtCore.QRect(20, 240, 66, 17))
+		self.label_6.setObjectName(_fromUtf8("label_6"))
+		self.label_7 = QtGui.QLabel(Dialog)
+		self.label_7.setGeometry(QtCore.QRect(20, 280, 66, 17))
+		self.label_7.setObjectName(_fromUtf8("label_7"))
+		self.label_8 = QtGui.QLabel(Dialog)
+		self.label_8.setGeometry(QtCore.QRect(20, 320, 66, 17))
+		self.label_8.setObjectName(_fromUtf8("label_8"))
+		self.lineEdit = QtGui.QLineEdit(Dialog)
+		self.lineEdit.setGeometry(QtCore.QRect(140, 40, 113, 27))
+		self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+		self.lineEdit_2 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_2.setGeometry(QtCore.QRect(140, 80, 113, 27))
+		self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+		self.lineEdit_3 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_3.setGeometry(QtCore.QRect(140, 120, 113, 27))
+		self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+		self.lineEdit_4 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_4.setGeometry(QtCore.QRect(140, 160, 113, 27))
+		self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+		self.lineEdit_5 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_5.setGeometry(QtCore.QRect(140, 200, 113, 27))
+		self.lineEdit_5.setObjectName(_fromUtf8("lineEdit_5"))
+		self.lineEdit_6 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_6.setGeometry(QtCore.QRect(140, 240, 113, 27))
+		self.lineEdit_6.setObjectName(_fromUtf8("lineEdit_6"))
+		self.lineEdit_7 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_7.setGeometry(QtCore.QRect(140, 280, 113, 27))
+		self.lineEdit_7.setObjectName(_fromUtf8("lineEdit_7"))
+		self.lineEdit_8 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_8.setGeometry(QtCore.QRect(140, 320, 113, 27))
+		self.lineEdit_8.setObjectName(_fromUtf8("lineEdit_8"))
+		self.lineEdit_9 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_9.setGeometry(QtCore.QRect(280, 40, 113, 27))
+		self.lineEdit_9.setObjectName(_fromUtf8("lineEdit_9"))
+		self.lineEdit_10 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_10.setGeometry(QtCore.QRect(280, 80, 113, 27))
+		self.lineEdit_10.setObjectName(_fromUtf8("lineEdit_10"))
+		self.lineEdit_11 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_11.setGeometry(QtCore.QRect(280, 120, 113, 27))
+		self.lineEdit_11.setObjectName(_fromUtf8("lineEdit_11"))
+		self.lineEdit_12 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_12.setGeometry(QtCore.QRect(280, 160, 113, 27))
+		self.lineEdit_12.setObjectName(_fromUtf8("lineEdit_12"))
+		self.lineEdit_13 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_13.setGeometry(QtCore.QRect(280, 200, 113, 27))
+		self.lineEdit_13.setObjectName(_fromUtf8("lineEdit_13"))
+		self.lineEdit_14 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_14.setGeometry(QtCore.QRect(280, 240, 113, 27))
+		self.lineEdit_14.setObjectName(_fromUtf8("lineEdit_14"))
+		self.lineEdit_15 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_15.setGeometry(QtCore.QRect(280, 280, 113, 27))
+		self.lineEdit_15.setObjectName(_fromUtf8("lineEdit_15"))
+		self.lineEdit_16 = QtGui.QLineEdit(Dialog)
+		self.lineEdit_16.setGeometry(QtCore.QRect(280, 320, 113, 27))
+		self.lineEdit_16.setObjectName(_fromUtf8("lineEdit_16"))
+		self.label_9 = QtGui.QLabel(Dialog)
+		self.label_9.setGeometry(QtCore.QRect(160, 10, 66, 17))
+		self.label_9.setAlignment(QtCore.Qt.AlignCenter)
+		self.label_9.setObjectName(_fromUtf8("label_9"))
+		self.label_10 = QtGui.QLabel(Dialog)
+		self.label_10.setGeometry(QtCore.QRect(310, 10, 66, 17))
+		self.label_10.setAlignment(QtCore.Qt.AlignCenter)
+		self.label_10.setObjectName(_fromUtf8("label_10"))
+
+		self.max_lst = [self.lineEdit, self.lineEdit_2, self.lineEdit_3, self.lineEdit_4, self.lineEdit_5, self.lineEdit_6, self.lineEdit_7, self.lineEdit_8]
+
+		self.min_lst = [self.lineEdit_9, self.lineEdit_10, self.lineEdit_11, self.lineEdit_12, self.lineEdit_13, self.lineEdit_14, self.lineEdit_15, self.lineEdit_16]
+	
+		self.retranslateUi(Dialog)
+		QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+	def retranslateUi(self, Dialog):
+		Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+		self.pushButton.setText(QtGui.QApplication.translate("Dialog", "확인", None, QtGui.QApplication.UnicodeUTF8))
+		self.label.setText(QtGui.QApplication.translate("Dialog", "Graph 1", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_2.setText(QtGui.QApplication.translate("Dialog", "Graph 2", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_3.setText(QtGui.QApplication.translate("Dialog", "Graph 3", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_4.setText(QtGui.QApplication.translate("Dialog", "Graph 4", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_5.setText(QtGui.QApplication.translate("Dialog", "Graph 5", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_6.setText(QtGui.QApplication.translate("Dialog", "Graph 6", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_7.setText(QtGui.QApplication.translate("Dialog", "Graph 7", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_8.setText(QtGui.QApplication.translate("Dialog", "Graph 8", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_9.setText(QtGui.QApplication.translate("Dialog", "Max", None, QtGui.QApplication.UnicodeUTF8))
+		self.label_10.setText(QtGui.QApplication.translate("Dialog", "Min", None, QtGui.QApplication.UnicodeUTF8))
+
+class DataStat_Dialog(QtGui.QDialog):
+	def __init__(self, values):
+		self.dialog = QtGui.QDialog()
+		self.ui = Ui_Dialog()
+		self.ui.setupUi(self.dialog)
+
+		self.ui.pushButton.clicked.connect(lambda:QtGui.QDialog.reject(self.dialog))
+		
+		self.setValue(values)
+		self.dialog.show()
+		self.dialog.exec_()
+
+	def setValue(self, values):
+		for (value, line) in zip(values['max'], reversed(self.ui.max_lst)):
+			line.setText(str(value))
+
+		for (value, line) in zip(values['min'], reversed(self.ui.min_lst)):
+			line.setText(str(value))

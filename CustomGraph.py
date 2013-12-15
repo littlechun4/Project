7ab846a2c16ad82d9798ab81a99dcef461bbba55
DifferentSC <gyewon@gyewon-VirtualBox.(none)>
2013-12-16 01:12:58 +0900

    Level0그래프의 오토레인지 비활성화

--- CustomGraph.py
+++ CustomGraph.py
@@ -8,8 +8,9 @@ import time
 from bisect import bisect_left
 from pyqtgraph import QtCore, QtGui
 
-#정렬된 타임스탬프의 레인지를 입력받아 그 인덱스의 레인지를 출력해주는 코드
+
 def getListRange(start, end, lst):
+    '''정렬된 타임스탬프의 레인지를 입력받아 그 인덱스의 레인지를 출력해주는 코드'''
 
     m = 0
     n = len(lst)-1
@@ -83,20 +84,22 @@ class CustomGraph(pg.GraphicsObject):
         self.y_max = []
         for i in reversed(range(len(widget_lst))):
             if (i != 7):
-                #axis = CustomAxis(orientation='bottom')
-                #widget_lst[i].addItem(axis)
+                #그래프를 그리고 XRange를 설정
                 widget_lst[i].plot(x=times, y=data)
                 widget_lst[i].setXRange(*self.region_lst[6-i].getRegion(), padding=0)
 
+                #XRange를 포함하는 Data의 index range를 구함
                 lst_range = getListRange(widget_lst[i].getViewBox().viewRange()[0][0], widget_lst[i].getViewBox().viewRange()[0][1], times)
                 new_data = data[lst_range[0]:lst_range[1]+1]
-                self.y_min += [min(new_data)]
-                self.y_max += [max(new_data)]
+
+                #Min, Max값을 구하고 이에 따른 YRange 설정
                 widget_lst[i].setYRange(min(new_data), max(new_data), padding=0)
 
+                #초기 Region설정
                 g_start = widget_lst[i].getViewBox().viewRange()[0][0]
                 g_end = widget_lst[i].getViewBox().viewRange()[0][1]
                 width = g_end - g_start
+
                 region = pg.LinearRegionItem([g_start + 2*width/5, g_start + 3*width/5])
                 region.setZValue(-10)
                 widget_lst[i].addItem(region)
@@ -104,16 +107,16 @@ class CustomGraph(pg.GraphicsObject):
                 self.old_region_lst+=[[region.getRegion()[0], region.getRegion()[1]]]
 
             else:
-                #axis = CustomAxis(orientation='bottom')
-                #widget_lst[i].addItem(axis)
+                #그래프를 그리고 XRange를 설정
                 widget_lst[i].plot(x=times, y=data)
                 widget_lst[i].setXRange(times[0], times[len(times)-1], padding = 0)
-                #widget_lst[i].setAutoPan(y=True)
-                widget_lst[i].getViewBox().enableAutoRange(axis=widget_lst[i].getViewBox().YAxis, enable=self.autorange)
-                self.y_min += [0]
-                self.y_max += [0]
+                #widget_lst[i].getViewBox().enableAutoRange(axis=widget_lst[i].getViewBox().YAxis, enable=self.autorange)
+                
+                #YRange 설정
+                widget_lst[i].setYRange(min(data), max(data), padding=0)
 
                 time_interval = times[len(times)-1] - times[0]
+                #초기 Region설정
                 region = pg.LinearRegionItem([times[0] + 2*time_interval/5, times[0] + 3*time_interval/5])
                 region.setZValue(-10)
                 widget_lst[i].addItem(region)
@@ -125,6 +128,7 @@ class CustomGraph(pg.GraphicsObject):
         # move의 정도를 계산하기 위해 예전 region값을 넣어 놓는다.
         self.old_region_lst.reverse()
 
+        #region이 변했을 때 자동으로 updatePlot이 불려지도록 연결시켜놓는다.
         self.region_lst[7].sigRegionChanged.connect(lambda: self.updatePlot(7, widget_lst))
         self.region_lst[6].sigRegionChanged.connect(lambda: self.updatePlot(6, widget_lst))
         self.region_lst[5].sigRegionChanged.connect(lambda: self.updatePlot(5, widget_lst))
@@ -158,10 +162,10 @@ class CustomGraph(pg.GraphicsObject):
     def boundingRect(self):
         return QtCore.QRectF(self.picture.boundingRect())
 
-    # loop로 동작하여 모든 level의 그래프를 업데이트함
+    
     def updatePlot(self, level, widget_lst):
-        #Next curvearrow's pos
-        next_pos = 0
+
+        '''하나의 region이 변했을 때 자동으로 불러져서 변경사항을 처리함'''
 
         #lock이 걸려있으면 동작하지 않음
         if (self.pre_empt == 1):
@@ -173,11 +177,12 @@ class CustomGraph(pg.GraphicsObject):
         left_move = self.region_lst[level].getRegion()[0] - self.old_region_lst[level][0]
         right_move = self.region_lst[level].getRegion()[1] - self.old_region_lst[level][1]
 
-        # It's Scroll!
+        # 리전의 넓이가 변하지 않음 -> It's Scroll!
         if(left_move==right_move):
 
             for level in range(level, 0, -1):
 
+                #다음레벨 계산
                 next_level = level - 1
 
                 if (next_level==-1):
@@ -205,15 +210,12 @@ class CustomGraph(pg.GraphicsObject):
                     lst_range = getListRange(new_xrange[0], new_xrange[1], self.times)
                     new_data = self.data[lst_range[0]:lst_range[1]+1]
 
-                    self.y_min = min(new_data)
-                    self.y_max = max(new_data)
-
-                    widget_lst[next_level].setYRange(self.y_min, self.y_max, padding=0, update=True)
+                    widget_lst[next_level].setYRange(min(new_data), max(new_data), padding=0, update=True)
 
                 #다음 레벨의 Region변환 self.pre_empt가 설정되어 있으므로 트리거는 X!
                 self.region_lst[next_level].setRegion(next_region)
                 
-        #It's not Scroll!
+        #region의 넓이가 변함 -> It's not Scroll!
         else:
             #가장 상위 레벨의 그래프일때는 리전이 변환되어도 아무런 일도 안함
             if(level != 0):
@@ -229,15 +231,11 @@ class CustomGraph(pg.GraphicsObject):
                 #범위에 맞게 Y레인지 변환
                 lst_range = getListRange(new_xrange[0], new_xrange[1], self.times)
                 new_data = self.data[lst_range[0]:lst_range[1]+1]
-          
-                self.y_min = min(new_data)
-                self.y_max = max(new_data)
 
-                widget_lst[next_level].setYRange(self.y_min, self.y_max, padding=0, update=True)
+                widget_lst[next_level].setYRange(min(new_data), max(new_data), padding=0, update=True)
 
         #lock off
         self.pre_empt = 0
-        return next_pos
 
     def updateRegion(self, region, connect_graph):
         region.setRegion(connect_graph.getViewBox().viewRange()[0])
@@ -248,23 +246,26 @@ class CustomGraph(pg.GraphicsObject):
         for (width, region) in width_region_lst:
             region.setRegion(width)
 
-    # Scroll function
-    # operace once per 50ms
-    # level = 1 ~ 8
-
+    
     def scroll(self, widget, level, vel, curve_arrow):
-
+        ''' Scroll을 처리한다.
+        타이머에 의해 250ms마다 한번씩 호출됨
+        level = 1 ~ 8'''
+        #시작, 끝의 범위를 지정한다.
         g_start = widget.getViewBox().viewRange()[0][0]
         g_end = widget.getViewBox().viewRange()[0][1]
-
+        
+        #0.25초 단위로 움직임
         one_step = vel*250
 
+        #한번 Scroll후의 region 계산
         new_region = [self.region_lst[8-level].getRegion()[0] + one_step, self.region_lst[8-level].getRegion()[1] + one_step]
 
+        #Region범위를 벗어나면 스크롤을 멈춘다.
         if(new_region[1] > g_end):
             return False
 
-        #new_pos = ((new_region[0] + new_region[1])/2 - g_start)/(g_end - g_start)
+        #CurveArrow도 같이 움직임
         region_mid = (new_region[0] + new_region[1])/2
         new_index = bisect_left(self.times, region_mid)
         if(curve_arrow != 0):
@@ -276,18 +277,19 @@ class CustomGraph(pg.GraphicsObject):
 #           self.region_lst[8-level].setRegion(new_region)
 #           return False
 
+
+        #새로운 Region을 적용한다 -> 자동으로 updatePlot함수 호출!
         self.region_lst[8-level].setRegion(new_region)
         return True
 
-    # Triggered when Parameter double clicked
     def parameterScroll(self, target_x, widget_lst):
-
+        '''ROI와 Arrow가 double click되었을 때 대상이 가운데에 오도록 Region과 Xrange를 조정'''
         if(self.pre_empt == 1):
             return
 
         self.pre_empt = 1
 
-        #target_x(ROI의 좌표)가 가운데로 올 수 있도록 각 레벨의 Region과 XRange를 조정한다.
+        #target_x(ROI, Arrow의 좌표)가 가운데로 올 수 있도록 각 레벨의 Region과 XRange를 조정한다.
         for i in range(7, -1, -1):
             region_mid = (self.region_lst[i].getRegion()[0] + self.region_lst[i].getRegion()[1])/2
             self.region_lst[i].setRegion([self.region_lst[i].getRegion()[0] + (target_x - region_mid), self.region_lst[i].getRegion()[1] + (target_x - region_mid)])
@@ -300,11 +302,8 @@ class CustomGraph(pg.GraphicsObject):
                 #범위에 맞게 Y레인지 변환
                 lst_range = getListRange(new_xrange[0], new_xrange[1], self.times)
                 new_data = self.data[lst_range[0]:lst_range[1]+1]
-          
-                self.y_min = min(new_data)
-                self.y_max = max(new_data)
 
-                widget_lst[i-1].setYRange(self.y_min, self.y_max, padding=0, update=True)
+                widget_lst[i-1].setYRange(min(new_data), max(new_data), padding=0, update=True)
 
 
         self.pre_empt = 0
